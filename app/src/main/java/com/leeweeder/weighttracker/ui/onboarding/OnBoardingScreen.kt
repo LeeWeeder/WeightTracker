7653c94679b56e60b9bca93bbf4f3aa568a8c0e3
LeeWeeder <ljmaq239@gmail.com>
2024-06-17 09:00:34 +0800

    Position NumberKeyBoard to bottom

--- app/src/main/java/com/leeweeder/weighttracker/ui/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/leeweeder/weighttracker/ui/onboarding/OnBoardingScreen.kt
@@ -1,5 +1,6 @@
 package com.leeweeder.weighttracker.ui.onboarding
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,15 +17,22 @@ import com.leeweeder.weighttracker.ui.components.rememberNumberKeyBoardState
 
 @Composable
 fun OnBoardingScreen() {
-    Column(modifier = Modifier.fillMaxSize().statusBarsPadding()) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .statusBarsPadding()) {
         Text(text = "Set your goal!")
 
         val numberKeyBoardState = rememberNumberKeyBoardState(defaultValue = "0")
         Card {
             Text(text = numberKeyBoardState.value)
         }
-        NumberKeyBoard(state = numberKeyBoardState, modifier = Modifier.padding(horizontal = 8.dp)) {
-
+        Box(modifier = Modifier.fillMaxSize()) {
+            NumberKeyBoard(
+                state = numberKeyBoardState, modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .align(Alignment.BottomCenter)
+            ) {
+            }
         }
     }
 }
