8be87d9fd489cd4f744b09cb0464af61b32d845b
LeeWeeder <ljmaq239@gmail.com>
2024-06-17 08:52:28 +0800

    Add 8dp horizontal padding to NumberKeyBoard

--- app/src/main/java/com/leeweeder/weighttracker/ui/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/leeweeder/weighttracker/ui/onboarding/OnBoardingScreen.kt
@@ -2,12 +2,14 @@ package com.leeweeder.weighttracker.ui.onboarding
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.leeweeder.weighttracker.ui.components.NumberKeyBoard
 import com.leeweeder.weighttracker.ui.components.rememberNumberKeyBoardState
 
@@ -20,7 +22,7 @@ fun OnBoardingScreen() {
         Card {
             Text(text = numberKeyBoardState.value)
         }
-        NumberKeyBoard(state = numberKeyBoardState) {
+        NumberKeyBoard(state = numberKeyBoardState, modifier = Modifier.padding(horizontal = 8.dp)) {
 
         }
     }
