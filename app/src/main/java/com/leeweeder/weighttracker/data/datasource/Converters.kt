2caad1b7e410993666af26338f552d815781cf28
LeeWeeder <ljmaq239@gmail.com>
2024-06-21 06:34:20 +0800

    Remove unnecessary converter

--- app/src/main/java/com/leeweeder/weighttracker/data/datasource/Converters.kt
+++ app/src/main/java/com/leeweeder/weighttracker/data/datasource/Converters.kt
@@ -2,19 +2,8 @@ package com.leeweeder.weighttracker.data.datasource
 
 import androidx.room.TypeConverter
 import com.leeweeder.weighttracker.util.Weight
-import java.time.Instant
 
 class Converters {
-    @TypeConverter
-    fun fromTimestamp(date: Instant): Long {
-        return date.toEpochMilli()
-    }
-
-    @TypeConverter
-    fun toTimestamp(value: Long): Instant {
-        return Instant.ofEpochMilli(value)
-    }
-
     @TypeConverter
     fun fromWeight(weight: Weight): Double {
         return weight.value
